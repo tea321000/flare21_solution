c04541a28f6813f31e9b1fb81e27bbde8fbb83de
tea321000 <834914152@qq.com>
2021-09-07 08:55:14 +0800

    modify shell

--- download.sh
+++ download.sh
@@ -6,6 +6,6 @@ cd trained_weight
 pip install gdown
 if [ ! -f  "task500.zip" ]
 then
-gdown https://drive.google.com/uc?id=1YW8MsLaYUr6lhfpf_LL6kTelPiuJRhq9
+    gdown https://drive.google.com/uc?id=1YW8MsLaYUr6lhfpf_LL6kTelPiuJRhq9
 fi
 unzip task500.zip -d $RESULTS_FOLDER
--- predict.sh
+++ predict.sh
@@ -1,4 +1,1 @@
-export nnUNet_raw_data_base="./nnUNet_raw"
-export nnUNet_preprocessed="./nnUNet_preprocessed"
-export RESULTS_FOLDER="./nnUNet_trained_models"
-nnUNet_predict -i ./inputs -o ./predictions/ -tr nnUNetTrainerV2_flare -t 500 -m 3d_lowres -f 3
+nnUNet_predict -i ./inputs -o ./outputs/ -tr nnUNetTrainerV2_flare -t 500 -m 3d_lowres -f 3
